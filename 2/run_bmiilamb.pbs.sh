bdf7e4c599fcbef13c0fcd81aa78f6e345125ad4
Mark Piper <mark.piper@colorado.edu>
2017-04-24 10:12:12 -0600

    Add PBS commands to submission script

--- 2/run_bmiilamb.pbs.sh
+++ 2/run_bmiilamb.pbs.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
+#PBS -N ILAMB
+#PBS -M mark.piper@colorado.edu
+#PBS -m e
+#PBS -l pmem=8gb
+#PBS -l nodes=1:ppn=2
+#PBS -l walltime=12:00:00
 
 cd $PBS_O_WORKDIR
 source /home/csdms/wmt/_testing/conda/bin/activate
